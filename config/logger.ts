4f310f5b06042b7cdee5c2cde0908a10ae4a173e
pedrojaraujo <pedroa990@gmail.com>
2025-11-23 19:56:22 -0300

    refactor: remove transport configuration from logger setup

--- config/logger.ts
+++ config/logger.ts
@@ -1,16 +1,12 @@
-import { defineConfig, targets } from '@adonisjs/core/logger'
+import { defineConfig } from '@adonisjs/core/logger'
 
 const loggerConfig = defineConfig({
   default: 'app',
-
   loggers: {
     app: {
       enabled: true,
       name: 'application',
       level: 'info',
-      transport: {
-        targets: targets().push(targets.file({ destination: 1 })).toArray(),
-      },
     },
   },
 })
